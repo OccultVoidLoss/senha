dd5568997b96f850ad0f9ed1484cef792f565bbc
Void <luisgustavoromano@gmail.com>
2024-12-11 16:05:18 -0300

    Add files via upload

---
+++ atvemsala.py
@@ -0,0 +1,21 @@
+def ex1():
+    vocabulario = ["iteracao", "selecao", "controle"]
+    numeros = [17, 123]
+    vazia = []
+    lista_mista = ["ola", 2.0, 5*2, [10, 20]]
+
+    print(numeros)
+    print(lista_mista)
+    nova_lista = [numeros, vocabulario]
+    print(nova_lista)
+def ex3():
+    uma_lista =  ["ola", 2.0, 5, [10, 20]]
+    print(len(uma_lista))
+    print(len(['spam!', 1, ['Brie', 'Roquefort', 'Pol le Veq'], [1, 2, 3]]))
+def ex6():
+    numeros = [17, 123, 87, 34, 66, 8398, 44]
+    print(numeros[2])
+    print(numeros[9-8])
+    print(numeros[-2])
+    print(numeros[len(numeros)-1])
+
